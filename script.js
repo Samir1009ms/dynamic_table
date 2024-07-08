2881c99b6dd0aeb5f9def1e89a639b2c4bafd4fd
Samir1009ms <yusifovs1009@gmail.com>
2024-07-08 11:44:46 +0400

    update

---
+++ script.js
@@ -0,0 +1,541 @@
+const jsonData = [
+  {
+    name: "column 1",
+    font_weight: "bold",
+    font_style: "normal",
+    orientation: "horizontal",
+    text_align: "center",
+    font_size: "12",
+    place: "top",
+    is_sub: "0",
+    children: [
+      {
+        name: "column 1-1",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [
+          {
+            name: "column 1-1-1",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "0",
+            children: [
+              {
+                name: "column 1-1-1-1",
+                font_weight: "normal",
+                font_style: "normal",
+                orientation: "horizontal",
+                text_align: "center",
+                font_size: 12,
+                is_sub: "0",
+                children: [],
+              },
+            ],
+            id: 19,
+          },
+          {
+            name: "column 1-1-2",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "0",
+            children: [],
+            id: 20,
+          },
+        ],
+        id: 5,
+      },
+      {
+        name: "column 1-2",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [
+          {
+            name: "column 1-2-1",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "0",
+            children: [],
+            id: 21,
+          },
+          {
+            name: "column 1-2-2",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "0",
+            children: [],
+            id: 22,
+          },
+        ],
+        id: 6,
+      },
+    ],
+    id: 1,
+  },
+  {
+    name: "column 2",
+    font_weight: "bold",
+    font_style: "normal",
+    orientation: "horizontal",
+    text_align: "center",
+    font_size: "12",
+    place: "top",
+    is_sub: "0",
+    children: [],
+    id: 2,
+  },
+  {
+    name: "column 3",
+    font_weight: "bold",
+    font_style: "normal",
+    orientation: "vertical",
+    text_align: "center",
+    font_size: "12",
+    place: "top",
+    is_sub: "0",
+    children: [
+      {
+        name: "column 3-1",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "vertical",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "1",
+        children: [],
+        id: 24,
+      },
+      {
+        name: "column 3-2",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "vertical",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "1",
+        children: [],
+        id: 25,
+      },
+      {
+        name: "column 3-3",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "vertical",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [],
+        id: 26,
+      },
+    ],
+    id: 23,
+  },
+  {
+    name: "column 4",
+    font_weight: "normal",
+    font_style: "normal",
+    orientation: "horizontal",
+    text_align: "center",
+    font_size: 12,
+    place: "top",
+    is_sub: "0",
+    children: [
+      {
+        name: "column 4-1",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: 12,
+        is_sub: "0",
+        children: [],
+      },
+    ],
+  },
+  {
+    name: "row 1",
+    font_weight: "normal",
+    font_style: "normal",
+    orientation: "horizontal",
+    text_align: "left",
+    font_size: "12",
+    place: "left",
+    is_sub: "0",
+    children: [
+      {
+        name: "row 1-1",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [],
+        id: 7,
+      },
+      {
+        name: "row 1-2",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [],
+        id: 8,
+      },
+    ],
+    id: 3,
+  },
+  {
+    name: "row 2",
+    font_weight: "normal",
+    font_style: "normal",
+    orientation: "horizontal",
+    text_align: "left",
+    font_size: "12",
+    place: "left",
+    is_sub: "0",
+    children: [],
+    id: 4,
+  },
+  {
+    name: "row 3",
+    font_weight: "bold",
+    font_style: "normal",
+    orientation: "vertical",
+    text_align: "center",
+    font_size: "12",
+    place: "left",
+    is_sub: "0",
+    children: [
+      {
+        name: "row 3-1",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [
+          {
+            name: "row 3-1-1",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "1",
+            children: [],
+            id: 29,
+          },
+          {
+            name: "row 3-1-2",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "1",
+            children: [],
+            id: 30,
+          },
+          {
+            name: "row 3-1-3",
+            font_weight: "normal",
+            font_style: "normal",
+            orientation: "horizontal",
+            text_align: "center",
+            font_size: "12",
+            place: null,
+            is_sub: "1",
+            children: [],
+            id: 31,
+          },
+        ],
+        id: 28,
+      },
+      {
+        name: "row 3-2",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [],
+        id: 32,
+      },
+      {
+        name: "row 3-3",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: "12",
+        place: null,
+        is_sub: "0",
+        children: [],
+        id: 33,
+      },
+    ],
+    id: 27,
+  },
+  {
+    name: "row 4",
+    font_weight: "normal",
+    font_style: "normal",
+    orientation: "horizontal",
+    text_align: "left",
+    font_size: 12,
+    place: "left",
+    is_sub: "0",
+    children: [
+      {
+        name: "row 4-1",
+        font_weight: "normal",
+        font_style: "normal",
+        orientation: "horizontal",
+        text_align: "center",
+        font_size: 12,
+        is_sub: "0",
+        children: [],
+      },
+    ],
+  },
+];
+
+console.log(jsonData);
+
+function getMaxColAndRow(arr) {
+  const maxCounts = [];
+
+  function recursiveArr(arr, maxCount) {
+    if (arr.length != 0) {
+      maxCount++;
+      arr.forEach((item) => {
+        if (item.place == "top" || item.place == null) {
+          recursiveArr(item.children, maxCount);
+        }
+      });
+    } else {
+      maxCounts.push(maxCount);
+    }
+  }
+
+  recursiveArr(arr, 0);
+  return [maxCounts.length, Math.max(...maxCounts)];
+}
+let maxColAndRow = getMaxColAndRow(jsonData);
+
+function getCol(item) {
+  let colCount = 0;
+
+  function recursiveItem(item) {
+    if (item.children.length == 0) {
+      colCount++;
+    }
+    item.children.forEach((child) => {
+      if (child.children.length != 0) {
+        recursiveItem(child);
+      } else {
+        colCount++;
+      }
+    });
+  }
+
+  recursiveItem(item);
+
+  return colCount;
+}
+
+function getColsAndRows() {
+  let maxLimit = maxColAndRow[1];
+  let RowsAndCols = {};
+
+  function recursiveItem(item, maxLimit) {
+    let itemColNum = getCol(item);
+    if (item.children.length != 0) {
+      maxLimit--;
+      RowsAndCols[item.name] = [itemColNum, 1];
+      item.children.forEach((child) => {
+        recursiveItem(child, maxLimit);
+      });
+    } else {
+      RowsAndCols[item.name] = [itemColNum, maxLimit];
+    }
+  }
+
+  jsonData.forEach((item) => {
+    if (item.place == "top") {
+      recursiveItem(item, maxLimit);
+    }
+  });
+
+  return RowsAndCols;
+}
+
+const ColsAndRows = getColsAndRows();
+
+const dynamicTableEl = document.querySelector("#dynamicTable");
+const theadEl = dynamicTableEl.querySelector("thead");
+const tbodyEl = dynamicTableEl.querySelector("tbody");
+
+function setColsAndRows() {
+  let level = 0;
+  for (let i = 0; i < maxColAndRow[1]; i++) {
+    theadEl.innerHTML += `
+    <tr class="dynamic"></tr>
+    `;
+  }
+  const trElements = document.querySelectorAll("thead>tr.dynamic");
+  trElements[0].innerHTML += `
+  <td colspan="3" rowspan="4"></td>
+  <td colspan="1" rowspan="4">Setirler no</td>
+  `;
+
+  function recursiveItem(item, level) {
+    trElements[level].innerHTML += `
+      <td colspan="${ColsAndRows[item.name][0]}" rowspan="${ColsAndRows[item.name][1]
+      }">${item.name}</td>
+      `;
+    level++;
+    if (item.children.length != 0) {
+      item.children.forEach((child) => {
+        recursiveItem(child, level);
+      });
+    }
+  }
+
+  jsonData.forEach((item) => {
+    if (item.place == "top") {
+      recursiveItem(item, level);
+    }
+  });
+}
+
+setColsAndRows();
+
+function getColsAndRowsv2() {
+  let maxLimit = 3;
+  let RowsAndCols = {};
+
+  function recursiveItem(item, maxLimit) {
+    let itemColNum = getCol(item);
+    if (item.children.length != 0) {
+      maxLimit--;
+      RowsAndCols[item.name] = [1, itemColNum];
+      item.children.forEach((child) => {
+        recursiveItem(child, maxLimit);
+      });
+    } else {
+      RowsAndCols[item.name] = [maxLimit, itemColNum];
+    }
+  }
+
+  jsonData.forEach((item) => {
+    if (item.place == "left") {
+      recursiveItem(item, maxLimit);
+    }
+  });
+
+  return RowsAndCols;
+}
+
+const ColsAndRowsv2 = getColsAndRowsv2();
+console.log(ColsAndRowsv2);
+
+function setColsAndRowsv2() {
+  let level = 1;
+  for (let i = 0; i < maxColAndRow[0] + 1; i++) {
+    tbodyEl.innerHTML += `
+    <tr class="dynamic"></tr>
+    `;
+  }
+  const trElements = document.querySelectorAll("tbody>tr.dynamic");
+  trElements[0].innerHTML += `
+  <td colspan="3" rowspan="1">A</td>
+  <td colspan="1" rowspan="1">B</td>
+  <td colspan="1" rowspan="1">1</td>    
+  <td colspan="1" rowspan="1">2</td>
+  <td colspan="1" rowspan="1">3</td>
+  <td colspan="1" rowspan="1">4</td>
+  <td colspan="1" rowspan="1">5</td>
+  <td colspan="1" rowspan="1">5-1</td>
+  <td colspan="1" rowspan="1">5-2</td>
+  <td colspan="1" rowspan="1">6</td>
+  <td colspan="1" rowspan="1">7</td>
+  `;
+
+
+  function recursiveItem(item) {
+    if (item.children.length != 0) {
+      trElements[level].innerHTML += `
+      <td colspan="${ColsAndRowsv2[item.name][0]}" rowspan="${ColsAndRowsv2[item.name][1]
+        }">${item.name}</td>`;
+      item.children.forEach((child) => {
+        recursiveItem(child);
+      });
+    } else {
+      trElements[level].innerHTML += `
+      <td colspan="${ColsAndRowsv2[item.name][0]}" rowspan="${ColsAndRowsv2[item.name][1]
+        }">${item.name}</td>`;
+      level++;
+    }
+  }
+
+  jsonData.forEach((item) => {
+    if (item.place == "left") {
+      recursiveItem(item);
+    }
+  });
+  trElements.forEach((item, index) => {
+
+    if (index !== 0) {
+      item.innerHTML += `
+      <td>${index}</td>
+      `
+      console.log(item)
+      for (let i = 0; i < maxColAndRow[0]; i++) {
+        item.innerHTML += `
+      <td></td>`;
+      }
+    }
+  });
+}
+
+setColsAndRowsv2();
